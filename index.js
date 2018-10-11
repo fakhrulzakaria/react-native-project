db89cc6e8906a5ac2f7af6cee203af6f209e7975
fakhrulzakaria <41604094+fakhrulzakaria@users.noreply.github.com>
2018-10-11 23:05:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import Biodata from './components/Biodata';
+import Biodata from './components/Biodata'; {/* Ganti 2 tulisan Biodata */}
 
 
-AppRegistry.registerComponent('ProjekPertama', () => Biodata);
+AppRegistry.registerComponent('ProjekPertama', () => Biodata); {/* Ganti tulisan Biodata */}
